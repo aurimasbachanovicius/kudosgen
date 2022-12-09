fc50eccb97855bbf3399b9ec8e5bbd2ba972da9d
Aurimas Bachanovicius <aurimas.bachanovicius@gmail.com>
2022-12-09 16:34:09 +0200

    Use simpler version of svelte carcass

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,6 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 declare namespace App {
 	// interface Error {}
 	// interface Locals {}
