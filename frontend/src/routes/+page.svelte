c7d4da925509ce0d0afb786ccf95d65fb193f75f
Aurimas Bachanovicius <aurimas.bachanovicius@gmail.com>
2022-12-09 16:47:15 +0200

    working page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to Kudos Generation page</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="col-lg-8 mx-auto p-4 py-md-5">
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap"/>
+                </svg>
+                <span class="fs-4">Simple header</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            </ul>
+        </header>
+        <main>
+            <h1>Get started with Bootstrap</h1>
+            <p class="fs-5 col-md-8">Quickly and easily get started with Bootstrap's compiled, production-ready files with
+                this barebones example featuring some basic HTML and helpful links. Download all our examples to get
+                started.</p>
+
+            <div class="mb-5">
+                <a href="../examples/" class="btn btn-primary btn-lg px-4">Download examples</a>
+            </div>
+
+            <hr class="col-3 col-md-2 mb-5">
+
+            <div class="row g-5">
+                <div class="col-md-6">
+                    <h2>Starter projects</h2>
+                    <p>Ready to beyond the starter template? Check out these open source projects that you can quickly
+                        duplicate to a new GitHub repository.</p>
+                    <ul class="icon-list ps-0">
+                        <li class="d-flex align-items-start mb-1"><a href="https://github.com/twbs/bootstrap-npm-starter"
+                                                                     rel="noopener" target="_blank">Bootstrap npm
+                            starter</a></li>
+                        <li class="text-muted d-flex align-items-start mb-1">Bootstrap Parcel starter (coming soon!)</li>
+                    </ul>
+                </div>
+
+                <div class="col-md-6">
+                    <h2>Guides</h2>
+                    <p>Read more detailed instructions and documentation on using or contributing to Bootstrap.</p>
+                    <ul class="icon-list ps-0">
+                        <li class="d-flex align-items-start mb-1"><a href="../getting-started/introduction/">Bootstrap quick
+                            start guide</a></li>
+                        <li class="d-flex align-items-start mb-1"><a href="../getting-started/webpack/">Bootstrap Webpack
+                            guide</a></li>
+                        <li class="d-flex align-items-start mb-1"><a href="../getting-started/parcel/">Bootstrap Parcel
+                            guide</a></li>
+                        <li class="d-flex align-items-start mb-1"><a href="../getting-started/vite/">Bootstrap Vite
+                            guide</a></li>
+                        <li class="d-flex align-items-start mb-1"><a href="../getting-started/contribute/">Contributing to
+                            Bootstrap</a></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+        <footer class="pt-5 my-5 text-muted border-top">
+            Created by the Bootstrap team &middot; &copy; 2022
+        </footer>
+    </div>
+</div>
+
+
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
