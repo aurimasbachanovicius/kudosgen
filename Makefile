1c6c41fc3e7af5ae9672bae04a3a1d8a11f93018
Aurimas Bachanovicius <aurimas.bachanovicius@gmail.com>
2022-12-29 14:52:47 +0200

    svelte form

--- Makefile
+++
@@ -1,2 +0,0 @@
-run:
-	docker stop kudosgen; docker rm kudosgen; docker build -t kudosgen . && docker run --name kudosgen -d -p 8085:80 kudosgen:latest
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import '@picocss/pico'
+</script>
+
+<main class="container">
+    <header class="container">
+        <hgroup>
+            <h1>Kudosgen</h1>
+            <h2>Create KUDOS png easily.</h2>
+        </hgroup>
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+    <footer class="pt-5 my-5 text-muted border-top">
+            Created with ❤️ 2022
+    </footer>
+</main>
